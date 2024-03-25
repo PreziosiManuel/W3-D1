323912e0cab430d3099b5abd424e1741887dfcc7
PreziosiManuel <156680696+PreziosiManuel@users.noreply.github.com>
2024-03-25 17:33:20 +0100

    fa

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -4,44 +4,43 @@ import Job from "./Job";
 import { NavLink, useParams } from "react-router-dom";
 
 const CompanySearchResults = () => {
-	const [jobs, setJobs] = useState([]);
-	const params = useParams();
+  const [jobs, setJobs] = useState([]);
+  const params = useParams();
 
-	const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?company=";
+  const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?company=";
 
-	useEffect(() => {
-		getJobs();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  useEffect(() => {
+    getJobs();
+  }, []);
 
-	const getJobs = async () => {
-		try {
-			const response = await fetch(baseEndpoint + params.companyName);
-			if (response.ok) {
-				const { data } = await response.json();
-				setJobs(data);
-			} else {
-				alert("Error fetching results");
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const getJobs = async () => {
+    try {
+      const response = await fetch(baseEndpoint + params.companyName);
+      if (response.ok) {
+        const { data } = await response.json();
+        setJobs(data);
+      } else {
+        alert("Error fetching results");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<Container>
-			<Row>
-				<Col>
-					<NavLink className="nav-link btn btn-primary m-2" style={{ height: "30px", width: "60px" }} to={"/"}>
-						Home
-					</NavLink>
-					{jobs.map((jobData) => (
-						<Job key={jobData._id} data={jobData} />
-					))}
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <NavLink className="nav-link btn btn-primary m-2" style={{ height: "30px", width: "60px" }} to={"/"}>
+            Home
+          </NavLink>
+          {jobs.map((jobData) => (
+            <Job key={jobData._id} data={jobData} />
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default CompanySearchResults;
